2a036f2c8e8101789f1188dd423e45a30691aad7
TARIKERKAM <tedogan@hotmail.com>
2020-05-13 19:03:16 +0400

    update

--- src/day1/CreateChannel.java
+++ src/day1/CreateChannel.java
@@ -3,6 +3,12 @@ package day1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User is able to create channels");
+        int x= 0 ;
+        if (x==0){
+            System.out.println("equal");
+        }
+
+        //there is changes now in intellijy
     }
 
 }
