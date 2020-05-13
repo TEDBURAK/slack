f42bc224dff8c967e68f4cc9d4c8c82ce83537f3
TEDBURAK <62037830+TEDBURAK@users.noreply.github.com>
2020-05-13 19:09:18 +0400

    Update CreateChannel.java

--- src/day1/CreateChannel.java
+++ src/day1/CreateChannel.java
@@ -3,6 +3,19 @@ package day1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User is able to create channels");
+        
+        
+        
+        
+        
+        
+        
+        String s="a";
+        if(s.contains("a"){
+            System.out.println("contain");
+        }else{
+           System.out.println("Hello");
+        }
     }
 
 }
